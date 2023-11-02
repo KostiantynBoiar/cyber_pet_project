9fe6d47c8d13e413c8a5c740eacddd0cd2982b0c
BoyarKonstantin <opezdalya@gmail.com>
2023-11-02 16:49:24 +0000

    Created header's directory

---
+++ src/headers/csv_api.h
@@ -0,0 +1,8 @@
+//
+// Created by kostiantyn on 11/2/23.
+//
+
+#ifndef CYBER_PET_PROJECT_CSV_API_H
+#define CYBER_PET_PROJECT_CSV_API_H
+
+#endif //CYBER_PET_PROJECT_CSV_API_H
---
+++ src/headers/cyber_pet.h
@@ -0,0 +1,8 @@
+//
+// Created by kostiantyn on 11/2/23.
+//
+
+#ifndef CYBER_PET_PROJECT_CYBER_PET_H
+#define CYBER_PET_PROJECT_CYBER_PET_H
+
+#endif //CYBER_PET_PROJECT_CYBER_PET_H
