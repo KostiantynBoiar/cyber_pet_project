c0c3211b27d38a81f30dd4468d52c9c4ef70e521
BoyarKonstantin <opezdalya@gmail.com>
2023-11-04 11:27:38 +0000

    Commit

---
+++ src/headers/CSV.h
@@ -0,0 +1,8 @@
+//
+// Created by kostiantyn on 11/2/23.
+//
+
+#ifndef CYBER_PET_PROJECT_CSV_H
+#define CYBER_PET_PROJECT_CSV_H
+
+#endif //CYBER_PET_PROJECT_CSV_H
--- src/headers/csv_api.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by kostiantyn on 11/2/23.
-//
-
-#ifndef CYBER_PET_PROJECT_CSV_API_H
-#define CYBER_PET_PROJECT_CSV_API_H
-
-#endif //CYBER_PET_PROJECT_CSV_API_H
