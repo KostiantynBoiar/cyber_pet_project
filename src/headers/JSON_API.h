50a5a9cd74aca333274fa3ed36b5e5020dd62f4a
BoyarKonstantin <opezdalya@gmail.com>
2023-11-09 18:22:51 +0000

    Commit

--- src/headers/JSON_API.h
+++ src/headers/JSON_API.h
@@ -1,13 +1,16 @@
-//
-// Created by kostiantyn on 11/7/23.
-//
-
 #ifndef CYBER_PET_PROJECT_JSON_API_H
 #define CYBER_PET_PROJECT_JSON_API_H
 
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
 class JSON_API {
+private:
+    rapidjson::Document document;
+public:
+
 
 };
-
 
 #endif //CYBER_PET_PROJECT_JSON_API_H
